dfee84c3e9aa809c00784412d16a79dee0249ab6
dartunian <dartunian@me.com>
2020-11-14 20:16:12 -0800

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,7 +8,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
+<<<<<<< Updated upstream
         <h1>I am a gitter!</h1>
+=======
+        <h1>dev branch</h1>
+>>>>>>> Stashed changes
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
