d50a2307d23dcf0fc67622ca17f127afe0817002
dartunian <dartunian@me.com>
2020-11-15 18:30:08 -0800

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,7 +8,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Hi Taylor!</h1>
+        <h1>master branch</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
