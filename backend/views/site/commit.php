114d0aea0f319adf3ed2be7d68191321f45b6f4c
dartunian <dartunian@me.com>
2020-11-17 22:48:42 -0800

    Create commit.php

---
+++ backend/views/site/commit.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Commit';
+?>
+<div class="site-commit">
+    <h1><?= Html::encode($this->title) ?></h1>
+	
+</div>
