2091d80c6a98df75539f7926e6a5536c922900b8
dartunian <dartunian@me.com>
2020-11-18 15:42:45 -0800

    Update SiteController.php

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -84,7 +84,7 @@ class SiteController extends Controller
         {
             $oauthClient = new Google();
             $buildUrl = $oauthClient->buildAuthUrl(); // Build authorization URL
-            $url = $buildUrl."&client_id=".$oauthClient->clientId;
+            $url = $buildUrl."&client_id=".$oauthClient->cliented;
             //Yii::$app->getResponse()->redirect($url); // Redirect to authorization URL.
             echo $url;
             // After user returns at our site:
