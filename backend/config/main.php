b33ac02e03449ab116665630623e5cfb40c8e4ff
dartunian <dartunian@me.com>
2020-11-19 12:55:49 -0800

    Update main.php

--- backend/config/main.php
+++ backend/config/main.php
@@ -22,7 +22,7 @@ return [
                 'class' => 'yii\authclient\Collection',
                 'clients' => [
                     'google' => [
-                        'class' => 'yii\authclient\clients\google',
+                        'class' => 'yii\authclient\clients\Google',
                         'clientId' => $config['oauth_google_clientId'],
                         'clientSecret' => $config['oauth_google_clientSecret'],
                     ],
