fa20732175befdf5a2f0c2da4be9a2baf23eae52
dartunian <dartunian@me.com>
2020-11-20 22:26:34 -0800

    Update login.php

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -12,25 +12,16 @@ $this->title = 'Login';
 ?>
 <div class="col-md-4"></div>
 <div class="col-md-4">
-     <h1><?= Html::encode($this->title) ?></h1>
-<?php /* 
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-*/ ?>            
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h1><?= Html::encode($this->title) ?></h1>
+        </div>
+        <div class="panel-body">
             <a class="btn btn-lg btn-default" href="<?= Url::to(['site/auth']) ?>">
                 <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
                 Sign in with Google
             </a>
+        </div>
+    </div>
 </div>
 <div class="col-md-4"></div>
