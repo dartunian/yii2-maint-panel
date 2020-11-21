bd16af4d3c561ea4fde5903b0642160f0e581e43
dartunian <dartunian@me.com>
2020-11-20 22:07:02 -0800

    Update login.php

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -29,14 +29,8 @@ $this->title = 'Login';
 
             <?php ActiveForm::end(); ?>
 */ ?>            
-            <a class="btn btn-lg btn-success" href="<?= Url::to(['site/auth']) ?>">Login with Google</a>
-            
-            <div class="row">
-              <div class="col-md-3">
-                <a class="btn btn-outline-dark" href="/users/googleauth" role="button" style="text-transform:none">
-                  <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-                  Login with Google
-                </a>
-              </div>
-            </div>       
+            <a class="btn btn-lg btn-default" href="<?= Url::to(['site/auth']) ?>">
+                <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                Login with Google
+            </a>
 </div>
