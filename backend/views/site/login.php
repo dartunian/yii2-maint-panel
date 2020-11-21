29f78600b671ff947838b18999303610bde49a3c
dartunian <dartunian@me.com>
2020-11-20 22:05:15 -0800

    Update login.php

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -31,19 +31,12 @@ $this->title = 'Login';
 */ ?>            
             <a class="btn btn-lg btn-success" href="<?= Url::to(['site/auth']) ?>">Login with Google</a>
             
-            <div class="col s12 m6 offset-m3 center-align">
-                <a class="oauth-container btn darken-4 white black-text" href="/users/google-oauth/" style="text-transform:none">
-                    <div class="left">
-                        <img width="20px" style="margin-top:7px; margin-right:8px" alt="Google sign-in" 
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-                    </div>
-                    Login with Google
+            <div class="row">
+              <div class="col-md-3">
+                <a class="btn btn-outline-dark" href="/users/googleauth" role="button" style="text-transform:none">
+                  <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                  Login with Google
                 </a>
-            </div>
-            
-            <!-- Compiled and minified CSS -->
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-            
-            <!-- Compiled and minified JavaScript -->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>                   
+              </div>
+            </div>       
 </div>
