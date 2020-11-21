e1e03a844c6864799e4d09ee24e1a60486bb866c
dartunian <dartunian@me.com>
2020-11-20 22:27:13 -0800

    Update login.php

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -14,7 +14,7 @@ $this->title = 'Login';
 <div class="col-md-4">
     <div class="panel panel-default">
         <div class="panel-heading">
-            <h1><?= Html::encode($this->title) ?></h1>
+            <?= Html::encode($this->title) ?>
         </div>
         <div class="panel-body">
             <a class="btn btn-lg btn-default" href="<?= Url::to(['site/auth']) ?>">
