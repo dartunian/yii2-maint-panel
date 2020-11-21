849a7b014491e8e51a7bfe40e3aa445a5e0ef6e1
dartunian <dartunian@me.com>
2020-11-21 15:50:15 -0800

    Update users.php

--- backend/views/site/users.php
+++ backend/views/site/users.php
@@ -8,6 +8,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
 use common\models\User;
+use kartik\grid\GridView;
 
 $this->title = 'Users';
 ?>
@@ -18,11 +19,49 @@ $this->title = 'Users';
         </div>
         <div class="panel-body">
 			<?php
-			$getUsers = User::find()->all();
-			
-			foreach($getUsers as $user)
-				echo $user->username;
-			endforeach;
+			$gridColumns = [
+				['class' => 'kartik\grid\CheckboxColumn']
+			];
+			echo GridView::widget([
+				'id' => 'kv-grid-demo',
+				//'dataProvider' => $dataProvider,
+				//'filterModel' => $searchModel,
+				'columns' => $gridColumns, // check the configuration for grid columns by clicking button above
+				'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
+				'headerRowOptions' => ['class' => 'kartik-sheet-style'],
+				'filterRowOptions' => ['class' => 'kartik-sheet-style'],
+				'pjax' => true, // pjax is set to always true for this demo
+				// set your toolbar
+				'toolbar' =>  [
+					['content' => 
+						Html::button('<i class="glyphicon glyphicon-plus"></i>', ['type' => 'button', 'title' => Yii::t('kvgrid', 'Add Book'), 'class' => 'btn btn-success', 'onclick' => 'alert("This will launch the book creation form.\n\nDisabled for this demo!");']) . ' '.
+						Html::a('<i class="glyphicon glyphicon-repeat"></i>', ['grid-demo'], ['data-pjax' => 0, 'class' => 'btn btn-default', 'title' => Yii::t('kvgrid', 'Reset Grid')])
+					],
+					'{export}',
+					'{toggleData}',
+				]
+				// set export properties
+				/*
+				'export' => [
+					'fontAwesome' => true
+				],
+				// parameters from the demo form
+
+				'bordered' => $bordered,
+				'striped' => $striped,
+				'condensed' => $condensed,
+				'responsive' => $responsive,
+				'hover' => $hover,
+				'showPageSummary' => $pageSummary,
+				*/
+				'panel' => [
+					'type' => GridView::TYPE_PRIMARY,
+					'heading' => $this->title,
+				],
+				'persistResize' => false,
+				'toggleDataOptions' => ['minCount' => 10]
+				//'exportConfig' => $exportConfig,
+			]);
 			?>
         </div>
     </div>
