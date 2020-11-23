3a069cc4b30d3dae3d5c9b7eacf873f78d15220a
dartunian <dartunian@me.com>
2020-11-22 16:50:58 -0800

    Update about.php

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -21,7 +21,7 @@ echo FormGrid::widget([
     'autoGenerateColumns' => true,
     'rows' => [
         [
-            'contentBefore' => '<legend class="text-info"><small>Account Info</small></legend>',
+            'contentBefore' => '<legend class="text-info"><small>New Maintenance Request</small></legend>',
             'attributes' => [       // 2 column layout
                 'username' => ['label' => 'Name', 'type' => Form::INPUT_TEXT, 'options' => ['placeholder' => 'Enter name...']],
                 'type' => ['label' => 'Request type', 'type' => Form::INPUT_DROPDOWN_LIST, 'items' => [1 => 'test'], 'hint' => 'Select a request type...'],                
@@ -30,49 +30,6 @@ echo FormGrid::widget([
         [
             'attributes' => [
                 'notes' => ['label' => 'Additional information', 'type' => Form::INPUT_TEXTAREA, 'options' => ['placeholder' => 'Enter additional information...']],
-            ],
-        ],
-        [
-            'contentBefore' => '<legend class="text-info"><small>Profile Info</small></legend>',
-            'columns' => 3,
-            'autoGenerateColumns' => false, // override columns setting
-            'attributes' => [       // colspan example with nested attributes
-                'address_detail' => [ 
-                    'label' => 'Address',
-                    'columns' => 6,
-                    'attributes' => [
-                        'address' => [
-                            'type' => Form::INPUT_TEXT, 
-                            'options' => ['placeholder' => 'Enter address...'],
-                            'columnOptions' => ['colspan' => 3],
-                        ],
-                        'zip_code' => [
-                            'type' => Form::INPUT_TEXT, 
-                            'options' => ['placeholder' => 'Zip...'],
-                            'columnOptions' => ['colspan' => 2],
-                        ],
-                        'phone' => [
-                            'type' => Form::INPUT_TEXT, 
-                            'options' => ['placeholder'=>'Phone...']
-                        ],
-                    ]
-                ]
-            ],
-        ],
-        [
-            'attributes' => [       // 3 column layout
-                'rememberMe' => [   // radio list
-                    'type' => Form::INPUT_RADIO_LIST, 
-                    'items' => [true => 'Yes', false => 'No'], 
-                    'options' => ['inline' => true]
-                ],
-                'actions' => [    // embed raw HTML content
-                    'type' => Form::INPUT_RAW, 
-                    'value' => '<div style="text-align: right; margin-top: 20px">' . 
-                        Html::resetButton('Reset', ['class' => 'btn btn-default']) . ' ' .
-                        Html::submitButton('Submit', ['class' => 'btn btn-primary']) . 
-                        '</div>'
-                ],
             ],
         ],
     ]
