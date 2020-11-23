02eb10cf3371890bee11990e6747f2f6b2ae6ffa
dartunian <dartunian@me.com>
2020-11-23 13:40:37 -0800

    Update User.php

--- common/models/User.php
+++ common/models/User.php
@@ -50,6 +50,13 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * {@inheritdoc}
      */
+    public function rules()
+    {
+        return [
+            ['status', 'default', 'value' => self::STATUS_INACTIVE],
+            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
+        ];
+    }
 
     /**
      * {@inheritdoc}
