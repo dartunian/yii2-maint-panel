eae71a6bda4232c894c6cd071740a29bd540f151
dartunian <dartunian@me.com>
2020-11-26 13:36:10 -0800

    Update MaintenanceRequest.php

--- common/models/MaintenanceRequest.php
+++ common/models/MaintenanceRequest.php
@@ -4,7 +4,14 @@ namespace common\models;
 use yii\db\ActiveRecord;
 
 class MaintenanceRequest extends ActiveRecord
-{    
+{
+    public function rules()
+    {
+        return [
+            // the name, email, subject and body attributes are required
+            [['name', 'type', 'notes'], 'required'],
+        ];
+    }
     /**
      * @return string the name of the table associated with this ActiveRecord class.
      */
