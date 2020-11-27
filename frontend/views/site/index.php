c2a6f9b019ee7b59ad449b50448bf66d5dae0ba1
dartunian <dartunian@me.com>
2020-11-26 21:34:23 -0800

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -82,7 +82,7 @@ $this->title = 'Maintenance Panel';
             'vAlign' => 'middle',
             'mergeHeader' => true,
             'contentOptions' => [
-                'style'=>'max-width:35%; overflow: auto; white-space: normal; word-wrap: break-word;'
+                'style'=>'max-width: 200px; overflow: auto; white-space: normal; word-wrap: break-word;'
             ],
             'value' => function($data){return $data->notes;},
         ],
