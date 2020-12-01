1616f8fc015fc504f4c92acbb24b2e5cc704e7b2
dartunian <dartunian@me.com>
2020-12-01 15:36:55 -0800

    Update _libexpand.php

--- frontend/views/site/_libexpand.php
+++ frontend/views/site/_libexpand.php
@@ -24,7 +24,7 @@ $this->registerJs($script2, \yii\web\View::POS_READY);
 */
 ?>
 <div class="wrap">
-	<div class="col-md-4">
+	<div class="col-md-4" style="display:inline-block;word-break:break-all;">
 		<label class="control-label">Summary</label>
 		<div class='panel panel-default'>
 			<div class='panel-body'>
