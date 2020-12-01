84aa6912a40982e4b9ab47c52c98fc8b874e4942
dartunian <dartunian@me.com>
2020-12-01 14:05:01 -0800

    Update _libexpand.php

--- frontend/views/site/_libexpand.php
+++ frontend/views/site/_libexpand.php
@@ -3,4 +3,27 @@
 use common\models\User;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Url;
+
+/*
+$script2 = <<< JS
+function addUser(id, file)
+{
+	$.ajax({
+		type: 'POST',
+		url:'$toSU',
+		showNoty: false, // add this for disable notification
+		data:{id:id, file:file},
+		success:function(data){
+			$.pjax.reload({container: '#library_grid-pjax', timeout:5000});		
+		},
+	});
+}
+JS;
+
+$this->registerJs($script2, \yii\web\View::POS_READY);
+*/
 ?>
+<label class="control-label">Summary</label>
+<div class='panel panel-default'>
+	<?= $model->summary ?>
+</div>
