2fbc3600f350fa69147006d16ccad9167279c195
dartunian <dartunian@me.com>
2020-12-01 14:19:43 -0800

    Update _libexpand.php

--- frontend/views/site/_libexpand.php
+++ frontend/views/site/_libexpand.php
@@ -26,6 +26,6 @@ $this->registerJs($script2, \yii\web\View::POS_READY);
 <label class="control-label">Summary</label>
 <div class='panel panel-default'>
 	<div class='panel-body'>
-		<div style="overflow-wrap: break-word;"><?= chunk_split($model->summary, 10) ?></div>
+		<div style="overflow-wrap: break-word;"><?= chunk_split($model->summary, 10, "\n") ?></div>
 	</div>
 </div>
