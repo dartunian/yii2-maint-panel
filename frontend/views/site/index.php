7315bc17f3c95eb648f998600f095f2d5a978b1c
dartunian <dartunian@me.com>
2020-12-02 15:52:32 -0800

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -71,7 +71,20 @@ $this->title = 'Maintenance Panel';
             'detail' => function ($model, $key, $index, $column) {
                 return Yii::$app->controller->renderPartial('_libexpand', ['model' => $model, 'class' => 'warning']);
              },
-            'detailRowCssClass' => GridView::TYPE_WARNING,
+            'detailRowCssClass' => function($data){
+                if($data->status == 0)
+                {
+                    return GridView::TYPE_INFO;
+                }
+                elseif($data->status == 1)
+                {
+                    return GridView::TYPE_WARNING;
+                }
+                elseif($data->status == 2)
+                {
+                    return GridView::TYPE_SUCCESS
+                }
+        },         
             'headerOptions' => ['class' => 'kartik-sheet-style'],
             'expandOneOnly' => true,
             'expandIcon' => '',
