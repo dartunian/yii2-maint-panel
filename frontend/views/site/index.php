440a6c7c5fec2f9b071e33d5874db8e50cd29b84
dartunian <dartunian@me.com>
2020-12-02 13:16:55 -0800

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -71,10 +71,13 @@ $this->title = 'Maintenance Panel';
             'detail' => function ($model, $key, $index, $column) {
                 return Yii::$app->controller->renderPartial('_libexpand', ['model' => $model]);
              },
-            //'headerOptions' => ['class' => 'kartik-sheet-style'],
+            'headerOptions' => ['class' => 'kartik-sheet-style', 'style' => 'white-space: normal;'],
             'expandOneOnly' => true,
             'expandIcon' => '',
             'collapseIcon' => '',
+            'contentOptions' => [
+               'style'=>'white-space: normal;'
+            ],
         ],
         [
             'label' => '#',
